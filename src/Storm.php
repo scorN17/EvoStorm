4f2d8a746e24c02422a86c494c23fbad2b16c3c1
Сергей <scorn17@gmail.com>
2019-01-06 13:07:04 +0300

    Update Storm.php

--- src/Storm.php
+++ src/Storm.php
@@ -1,10 +1,22 @@
 <?php
 namespace EvoStorm\EvoStorm;
 
-class Storm
+class Storm extends \EvolutionCMS\Core
 {
 	public function __construct()
 	{
-		return 'test';
+		return parent::__construct();
+	}
+	
+	public function getDocumentIdentifier($method)
+	{
+		$url = parent::getDocumentIdentifier($method);
+		// if ($method == 'alias') {
+		// 	$url = $this->c->parseURLParams($url);
+		// 	if ($this->sendErrorPage) {
+		// 		$this->sendErrorPage();
+		// 	}
+		// }
+		return $url;
 	}
 }
